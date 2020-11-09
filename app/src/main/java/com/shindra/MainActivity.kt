7247a43925169e6cd17d6bf0c2c16d27406ec1b6
estel <estellem148@gmail.com>
2020-11-09 18:53:19 +0100

    test

--- app/src/main/java/com/shindra/MainActivity.kt
+++ app/src/main/java/com/shindra/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: ViewModel by viewModels()
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
