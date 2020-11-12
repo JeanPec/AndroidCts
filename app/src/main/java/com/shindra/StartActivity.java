ce43be4a48ad04568bd130bc09393e9aa28f5791
theocde <T150696lc100298h>
2020-11-12 15:50:40 +0100

    test-push

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class StartActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
