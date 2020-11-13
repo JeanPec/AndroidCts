712da538864371a284703e79c669ecaa56cbad4f
Simon WATHLE <simonwathle@hotmail.com>
2020-11-13 17:58:26 +0100

    Test - Testing of recycled view for Line views -> Success.

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -6,14 +6,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.shindra.arrakis.observable.ObservableExtensionKt;
 import com.shindra.arrakis.observable.ObservableListener;
+import com.shindra.ctslibrary.apibo.Coordinate;
+import com.shindra.ctslibrary.apibo.RouteType;
 import com.shindra.ctslibrary.bo.Line;
+import com.shindra.ctslibrary.bo.Stop;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class StartActivity extends AppCompatActivity
 {
@@ -21,11 +27,20 @@ public class StartActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_start);
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
 
+        ArrayList<Line> test = new ArrayList<Line>();
+        test.add(new Line("Ligne A", RouteType.BUS, null));
+        test.add(new Line("Ligne B", RouteType.BUS, null));
+        test.add(new Line("Ligne C", RouteType.BUS, null));
+        test.add(new Line("Ligne D", RouteType.BUS, null));
+
+
+        RecyclerView lineViews = findViewById(R.id.line_views);
+        lineViews.setLayoutManager(new LinearLayoutManager(this));
+        lineViews.setAdapter(new LineAdapter(test));
 
         ObservableExtensionKt.observe(model.lines(), new ObservableListener<ArrayList<Line>>()
         {
@@ -39,6 +54,7 @@ public class StartActivity extends AppCompatActivity
             public void onSuccess(ArrayList<Line> data)
             {
                 //call once the network call has responded with a success
+                int a = 5;
             }
 
             @Override
