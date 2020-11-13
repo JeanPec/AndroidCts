6b34bb81d705d3cb997bf15fc05899fdabc5b222
lolo067 <46133551+lolo067@users.noreply.github.com>
2020-11-13 17:34:10 +0100

    TPseance1

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -1,11 +1,15 @@
 package com.shindra;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.shindra.arrakis.observable.ObservableExtensionKt;
 import com.shindra.arrakis.observable.ObservableListener;
@@ -21,7 +25,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("info", "je suis la startactivity");
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
 
@@ -30,16 +34,30 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onLoading() {
                 //call once we started the network called. Indicate that the network call is in progress
+                Log.d("info", "message attente a afficher");
+                /*
+                Training for fragment :
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                PremierFragment fragment = new PremierFragment();
+                fragmentTransaction.add(R.id.fragment_container_view, fragment);
+                */
             }
 
             @Override
             public void onSuccess(ArrayList<Line> data) {
                 //call once the network call has responded with a success
-            }
+                Log.d("info", "appel à API reussi");
+                Log.d("info", model.toString());
+
+                setContentView(R.layout.liste_trams);
+                RecyclerView recList = (RecyclerView) findViewById(R.id.liste);
+           }
 
             @Override
             public void onError(@NotNull Throwable throwable) {
                 //call if the network call has responded with an error
+                Log.d("=erreur", "Erreur appel API");
             }
         });
     }
