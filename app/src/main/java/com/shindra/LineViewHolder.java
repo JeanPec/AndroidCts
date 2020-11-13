dcb0afd0ef65c6f7c21e17369246c77f5ad3e641
Gabrielle Huré <gabrielle.hure@students.fhnw.ch>
2020-11-13 17:29:51 +0100

    import des images et de la police, modif de couleur.xml, debut vue chargement et debut recycler view - a finir

---
+++ app/src/main/java/com/shindra/LineViewHolder.java
@@ -0,0 +1,17 @@
+package com.shindra;
+
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+public class LineViewHolder extends RecyclerView.ViewHolder {
+
+    private final ImageView lineName;
+    public LineViewHolder(@NonNull View itemView){
+        super(itemView);
+    }
+    public void onBind(Ligne ligne {
+
+    }
+//TODO finir le view holder
+}
--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.shindra.arrakis.observable.ObservableExtensionKt;
 import com.shindra.arrakis.observable.ObservableListener;
@@ -20,21 +21,28 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
-
-
         ObservableExtensionKt.observe(model.lines(), new ObservableListener<ArrayList<Line>>() {
             @Override
             public void onLoading() {
                 //call once we started the network called. Indicate that the network call is in progress
+                setContentView(R.layout.layout_chargement);
             }
 
             @Override
             public void onSuccess(ArrayList<Line> data) {
                 //call once the network call has responded with a success
+                setContentView(R.layout.activity_main);
+                RecyclerView recyclerView = findViewById(R.id.cardList);
+
+                recyclerView.setHasFixedSize(true);
+                LinearLayoutManager llmanager = new LinearLayoutManager(this);
+                llmanager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(llmanager);
+
+
             }
 
             @Override
