8d1d5a856d027a52c3ef86702a046f73a1c5a67e
tralhao <marcelo.tralhao@etu.unistra.fr>
2020-11-13 17:29:25 +0100

    add new file + beginning to implement recyclerView

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -1,11 +1,13 @@
 package com.shindra;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,11 +20,16 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 public class StartActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager llm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerView();
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
 
@@ -43,6 +50,14 @@ public class StartActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void initRecyclerView() {
+        //recyclerView = (RecyclerView) findViewById(R.id.cardligne);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
     }
 
 }
