6c2c2ea2613f8d3b06fd338091b84d105c2770e6
gael <ga.leclercq10@gmail.com>
2020-11-13 14:57:26 +0100

    test-push

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -40,6 +40,11 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onError(@NotNull Throwable throwable) {
                 //call if the network call has responded with an error
+
+
+
+
+
             }
         });
     }
