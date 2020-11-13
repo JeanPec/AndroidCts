f664d21733f4cdb88d2638a499726fad3cec89ff
Gabrielle Huré <gabrielle.hure@students.fhnw.ch>
2020-11-13 17:40:14 +0100

    modif de LineViewHolder - doit encore etre terminé

--- app/src/main/java/com/shindra/LineViewHolder.java
+++ app/src/main/java/com/shindra/LineViewHolder.java
@@ -7,11 +7,12 @@ import androidx.annotation.NonNull;
 public class LineViewHolder extends RecyclerView.ViewHolder {
 
     private final ImageView lineName;
+
     public LineViewHolder(@NonNull View itemView){
         super(itemView);
     }
-    public void onBind(Ligne ligne {
-
+    public void onBind(Ligne ligne){
+        lineName = itemView.findViewById(R.id.ic_a);
     }
 //TODO finir le view holder
 }
