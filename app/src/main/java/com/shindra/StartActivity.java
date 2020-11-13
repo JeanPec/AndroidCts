c3e807f578b6f6ce02d756f7f72ba4d8bcc7c342
Survetman <thesurvetman@gmail.com>
2020-11-13 09:39:52 +0100

    test-push-new-pc

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 
 public class StartActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
