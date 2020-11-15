1cb8c29f87a2532881a81eecfdb48fdef84b7cfb
Gabrielle Huré <gabrielle.hure@students.fhnw.ch>
2020-11-15 19:24:39 +0100

    modification place setCentent dans startActivity + recyclerview fonctionne - manque la recupération des données

--- app/src/main/java/com/shindra/LineViewHolder.java
+++ app/src/main/java/com/shindra/LineViewHolder.java
@@ -1,8 +1,15 @@
 package com.shindra;
 
+import android.view.View;
 import android.widget.ImageView;
 
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shindra.ctslibrary.bo.Line;
+
+import java.util.Objects;
 
 public class LineViewHolder extends RecyclerView.ViewHolder {
 
@@ -10,9 +17,31 @@ public class LineViewHolder extends RecyclerView.ViewHolder {
 
     public LineViewHolder(@NonNull View itemView){
         super(itemView);
+        lineName = itemView.findViewById(R.id.logo_ligne);
     }
-    public void onBind(Ligne ligne){
-        lineName = itemView.findViewById(R.id.ic_a);
+    public void onBind(Line ligne){
+        switch(ligne.getName()) {
+            case "A":
+                lineName.setImageResource(R.drawable.ic_tram_a);
+                break;
+            case "B":
+                lineName.setImageResource(R.drawable.ic_tram_b);
+                break;
+            case "C":
+                lineName.setImageResource(R.drawable.ic_tram_c);
+                break;
+            case "D":
+                lineName.setImageResource(R.drawable.ic_tram_d);
+                break;
+            case "E":
+                lineName.setImageResource(R.drawable.ic_tram_e);
+                break;
+            case "F":
+                lineName.setImageResource(R.drawable.ic_tram_f);
+                break;
+            default:
+                lineName.setImageResource(R.drawable.ic_tram);
+        }
+        // TODO modifier switch cqse qvec bon nom de ligne
     }
-//TODO finir le view holder
 }
--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -1,6 +1,7 @@
 package com.shindra;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +11,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.shindra.arrakis.observable.ObservableExtensionKt;
 import com.shindra.arrakis.observable.ObservableListener;
+import com.shindra.ctslibrary.apibo.RouteType;
 import com.shindra.ctslibrary.bo.Line;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_lines);
+
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerViewAdapter(getListOfLines()));
 
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
@@ -28,28 +38,33 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onLoading() {
                 //call once we started the network called. Indicate that the network call is in progress
-                setContentView(R.layout.layout_chargement);
+              //  setContentView(R.layout.layout_chargement);
+                Log.d("info", "!!!!!!! on loading");
             }
 
             @Override
             public void onSuccess(ArrayList<Line> data) {
+                Log.d("info", "!!!!!!! on success");
+                Log.d("info", String.valueOf(data));
+                //TODO recuperer les données et convertir en obj Line
                 //call once the network call has responded with a success
-                setContentView(R.layout.activity_main);
-                RecyclerView recyclerView = findViewById(R.id.cardList);
-
-                recyclerView.setHasFixedSize(true);
-                LinearLayoutManager llmanager = new LinearLayoutManager(this);
-                llmanager.setOrientation(LinearLayoutManager.VERTICAL);
-                recyclerView.setLayoutManager(llmanager);
-
-
+                recyclerView.getAdapter().notifyDataSetChanged();
             }
 
             @Override
             public void onError(@NotNull Throwable throwable) {
+                Log.d("info", "!!!!!!! on error");
                 //call if the network call has responded with an error
             }
         });
     }
+    private  ArrayList<Line> getListOfLines(){
+        ArrayList<Line> lines_l = new ArrayList<Line>();
+        lines_l.add(new Line("A", RouteType.TRAM,null));
+        lines_l.add(new Line("C", RouteType.TRAM,null));
+        lines_l.add(new Line("RSJHGH", RouteType.TRAM,null));
+        return lines_l;
+    }
+
 }
 
