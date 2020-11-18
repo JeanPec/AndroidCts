8e884ae53b73d7a8d37064c802272fb2c4c196b7
Julien Brenke <julien.brenke@gmail.com>
2020-11-18 21:02:00 +0100

    Implemented display gmap on fragmentMap

---
+++ app/src/main/java/com/shindra/Map/FragmentMap.kt
@@ -0,0 +1,14 @@
+package com.shindra.Map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.shindra.R
+import com.shindra.arrakis.controls.MapFragment
+
+class FragmentMap : MapFragment() {
+
+
+}
--- app/src/main/java/com/shindra/Map/MapActivity.kt
+++ app/src/main/java/com/shindra/Map/MapActivity.kt
@@ -2,16 +2,28 @@ package com.shindra.Map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.shindra.R
 import java.sql.DriverManager
+
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        val fragmentMap = FragmentMap()
+
+        val fragmentManager:FragmentManager = supportFragmentManager
+        val fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentMap, fragmentMap);
+        fragmentTransaction.commit();
+
+
         val intent = intent
-        val lineName = intent.getStringExtra("LINE_ID")
-        println("OUT $lineName")
+        val lineID = intent.getStringExtra("LINE_ID")
+        println("OUT $lineID")
+        title = "Ligne $lineID"
     }
 }
