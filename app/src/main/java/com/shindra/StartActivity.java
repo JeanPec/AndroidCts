9b5c511a5829da1a52e26bbc8247894d61fcb45d
Axel Meyer <axel.meyer2@etu.unistra.fr>
2020-11-19 12:08:49 +0100

    J'ai supprimé le fragment et j'ai réussi à afficher une recyclerview tirée d'un exemple sur internet. J'essaie d'adapter cet exemple pour afficher des cardview plutot que des textview sans succès pour le moment

--- app/src/main/java/com/shindra/StartActivity.java
+++ app/src/main/java/com/shindra/StartActivity.java
@@ -4,24 +4,40 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.cardview.widget.CardView;
 import androidx.lifecycle.ViewModelProvider;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.shindra.arrakis.observable.ObservableExtensionKt;
 import com.shindra.arrakis.observable.ObservableListener;
 import com.shindra.ctslibrary.bo.Line;
-
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class StartActivity extends AppCompatActivity {
 
+    RecyclerViewExempleAdapter adapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Nos Trams");
 
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.exemple_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewExempleAdapter(this, animalNames);
+        recyclerView.setAdapter(adapter);
 
         MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
 
