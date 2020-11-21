10450c0e2c7b1c7b55d353a9a356df894430a247
Gael Leclercq <ga.leclercq10@gmail.com>
2020-11-21 09:39:36 +0100

    Horaires finis, début de la map

--- app/src/main/java/com/shindra/ScheduleCardViewAdapter.kt
+++ app/src/main/java/com/shindra/ScheduleCardViewAdapter.kt
@@ -1,16 +1,19 @@
 package com.shindra
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.shindra.ctslibrary.bo.Line
 import com.shindra.ctslibrary.bo.Stop
 import java.util.*
 
-class ScheduleCardViewAdapter(line : Line?,callback : RecyclerItemClick):
+class ScheduleCardViewAdapter(line: Line?, callback: RecyclerItemClick):
     RecyclerView.Adapter<ScheduleCardViewAdapter.ScheduleCardViewHolder>() {
     var line:Line? = line
     var callback : RecyclerItemClick = callback
@@ -25,7 +28,7 @@ class ScheduleCardViewAdapter(line : Line?,callback : RecyclerItemClick):
     }
 
     override fun onBindViewHolder(holder: ScheduleCardViewHolder, position: Int) {
-        holder.onBind(line,line?.stops?.get(position))
+        holder.onBind(line, line?.stops?.get(position))
     }
 
     override fun getItemCount(): Int
@@ -33,31 +36,38 @@ class ScheduleCardViewAdapter(line : Line?,callback : RecyclerItemClick):
         var int :Int= line?.stops!!.size
         return int
     }
-    fun setStops(lineWithStops:Line?)
+    fun setStops(lineWithStops: Line?)
     {
         line = lineWithStops
     }
-    class ScheduleCardViewHolder(itemView: View, context:Context):RecyclerView.ViewHolder(itemView)
+    class ScheduleCardViewHolder(itemView: View, context: Context):RecyclerView.ViewHolder(itemView)
     {
         var context : Context = context
         var stopTextView : TextView = itemView.findViewById(R.id.stopTextView)
         var lineTextView : TextView = itemView.findViewById(R.id.lineTextView)
-        var hoursTextView : TextView = itemView.findViewById(R.id.lineTextView)
-        var indicationTextView : TextView = itemView.findViewById(R.id.lineTextView)
-        public fun  onBind(line:Line?,stop:Stop?)
+        var hoursTextView : TextView = itemView.findViewById(R.id.hoursTextView)
+        var indicationTextView : TextView = itemView.findViewById(R.id.indicationTextView)
+        public fun  onBind(line: Line?, stop: Stop?)
         {
+            val colorId = getColorId(context,"Ligne"+line?.name)
+            if(colorId != null)
+            {
+                lineTextView.setTextColor(ContextCompat.getColor(context, colorId))
+            }
             stopTextView.setText(stop?.name.toString())
-            lineTextView.setText("Ligne "+line?.name)
+            lineTextView.setText("Ligne " + line?.name)
             val date = stop?.estimatedArrivalTime
-            val timeStamp = date.time
             val calendar = Calendar.getInstance()
             calendar.time = date
-            cal.time = date
-            val hours = cal.get(Calendar.HOUR_OF_DAY)
-            val minutes = cal.get(Calendar.MINUTE)
-            hoursTextView.setText(hours.toString()+'H'+minutes.toString())
+            val hours = calendar.get(Calendar.HOUR_OF_DAY)
+            val minutes = String.format("%02d", calendar.get(Calendar.MINUTE))
+            hoursTextView.setText("$hours" + 'h' + "$minutes")
             indicationTextView.setText("Prochain départ")
         }
+        //Fonction permettant de récupérer l'id d'une couleur en fonction de son nom.
+        //Je pars sur la convention de nommage suivante, la couleur d'une ligne s'appellera tout le temps comme tel "Ligne+{nom de la ligne EN MAJUSCULE}
+        //Les conventions de nommage me permettent d'éviter un paquet de switchCase qui rendent le code peu maintenable à mon sens => si il y a X ligne de tram...
+        fun getColorId(context: Context, colorName: String) = context.getResources().getIdentifier("$colorName", "color", context.getPackageName())
     }
 
 }
