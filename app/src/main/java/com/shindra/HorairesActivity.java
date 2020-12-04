a753b10873883f5594bee4b7a7a723957682e326
Axel Meyer <axel.meyer2@etu.unistra.fr>
2020-12-04 20:52:05 +0100

    A

--- app/src/main/java/com/shindra/HorairesActivity.java
+++ app/src/main/java/com/shindra/HorairesActivity.java
@@ -1,12 +1,32 @@
 package com.shindra;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import com.shindra.ctslibrary.bo.Stop;
+
+import java.util.ArrayList;
 
 public class HorairesActivity extends AppCompatActivity {
 
-    //On implémente le constructeur (vide)
-    public HorairesActivity(){
+    //On crée la rcycler view qui va afficher les  ainsi que son adapter
+    RecyclerView RecyclerViewHoraires;
+    RecyclerView.Adapter RecyclerViewHorairesAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horaires);
+        setTitle("Horaire");
+
+        //On crée une liste qui va contenir les arrêts
+        ArrayList<Stop> ListeArrets = new ArrayList<>();
+
     }
+
 
 
 }
